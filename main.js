41a88266d167592ffda2f81a871010af5a2b97a4
Caitlyn Lucy Palm <caitlyn.palm@de.clara.net>
2025-06-24 14:05:13 +0200

    Section_2 _13_Save

---
+++ main.js
@@ -0,0 +1,12 @@
+
+document.querySelector("#open-nav-menu").addEventListener("click", function(){
+    document.querySelector("header nav .wrapper").classList.add("nav-open");
+});
+
+document.querySelector("#close-nav-menu").addEventListener("click", function(){
+    document.querySelector("header nav .wrapper").classList.remove("nav-open");
+});
+
+
+const customerName = "Thalia"; 
+let customerBalance = 2000;
